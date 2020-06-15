088b1e8b266cde5899c3cba09974232ebe1d928b
my9756jjh <62217709+my9756jjh@users.noreply.github.com>
2020-06-15 14:00:40 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+function easteregg() {
+  var x = 0;
+  alert("오 이스터에그 찾았ㄷ");
+  alert("ㅎㅎ 근데");
+  alert("함정임 ㅋㅋ");
+  alert("앜ㅋㅋㅋ 이걸속냐");
+  while (x < 1000) {
+    alert(1000 - x + "번 더 클릭하삼 응슷응");
+    x++;
+  }
+  var ha = confirm("그만하실?");
+  if (ha) {
+    alert("그만할줄 알았짘ㅋㅋㅋ");
+    alert("벌칙으로 1000번 추가임 응슷응");
+    var y = 0;
+    while (y < 2000) {
+      alert(2000 - y + "번 더 클릭하삼 응슷응");
+      y++;
+    }
+  } else {
+    alert("ㅇㅋ");
+    var z = 0;
+    while (z < 1000) {
+      alert(1000 - z + "번 더 클릭하삼 응슷응");
+      z++;
+    }
+  }
+  alert("고생했다 인간");
+  alert("친구 컴에서 해봐");
+  alert("물론 익스플로러로 실행하는거 까먹지 말고");
+  
+}
